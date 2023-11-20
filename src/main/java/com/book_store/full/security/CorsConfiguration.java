51ebea645540a1a66270894f0bf6c0ed8aeee755
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2023-11-20 15:58:40 +0200

    add cors and two endpoint

---
+++ src/main/java/com/book_store/full/security/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.book_store.full.security;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+            }
+        };
+    }
+}
--- src/main/java/com/book_store/full/security/SecurityConfig.java
+++ src/main/java/com/book_store/full/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -46,7 +47,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeRequests()
-                .requestMatchers("/home", "/home/addnewuser", "/home/authenticate")
+                .requestMatchers("/home", "/home/addnewuser", "/home/authenticate", "/home/topselling" , "/home/resentllyadded")
                 .permitAll()
                 .and()
                 .authorizeRequests()
@@ -57,6 +58,7 @@ public class SecurityConfig {
                 .and()
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors(Customizer.withDefaults())
                 .build();
     }
 
