6dd99dc3208b7a87adbee78bac9fe5dba58b2251
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-06-18 10:10:55 +0300

    chore: Update UserController to use UserUpdate DTO for updateUser method

--- src/main/java/com/book_store/full/controller/UserController.java
+++ src/main/java/com/book_store/full/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,10 @@ import com.book_store.full.dto.Order;
 import com.book_store.full.dto.userdto.UserUpdate;
 import com.book_store.full.services.UserService;
 import com.fasterxml.jackson.databind.JsonNode;
-
+import org.springframework.http.MediaType;
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping(path = "", produces = { MediaType.APPLICATION_JSON_VALUE })
 public class UserController {
 
     @Autowired
